cf332b73a56384eccece1d245a6505f3a87ae188
kfischi <kfir.biz@gmail.com>
2026-01-30 08:33:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,19 @@
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Gallery from '@/components/Gallery'
+import Testimonials from '@/components/Testimonials'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 
+export default function Home() {
+  return (
+    <main className="relative">
+      <Hero />
+      <About />
+      <Gallery />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
