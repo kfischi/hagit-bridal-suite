5ba575d8887574887817469d0651d094f6bf0d0e
kfischi <kfir.biz@gmail.com>
2026-01-31 19:46:18 +0200

    Create ScrollProgress.tsx

---
+++ components/ScrollProgress.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { motion, useScroll, useSpring } from 'framer-motion'
+
+export default function ScrollProgress() {
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  })
+
+  return (
+    <motion.div
+      className="fixed top-0 left-0 right-0 h-1 bg-gradient-to-r from-[#D4AF37] via-[#F5E6D3] to-[#D4AF37] origin-left z-[60] shadow-gold"
+      style={{ scaleX }}
+    />
+  )
+}
