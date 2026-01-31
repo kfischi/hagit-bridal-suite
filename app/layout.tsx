2ab8eb8eb4c82b90b782aa116d27a613db76ec5c
kfischi <kfir.biz@gmail.com>
2026-01-31 18:58:52 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,16 @@
 import type { Metadata } from 'next'
-import { Playfair_Display, Heebo } from 'next/font/google'
 import './globals.css'
-
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-playfair',
-  display: 'swap',
-})
-
-const heebo = Heebo({
-  subsets: ['hebrew', 'latin'],
-  variable: '--font-heebo',
-  display: 'swap',
-})
 
 export const metadata: Metadata = {
   title: 'חגית | התארגנות כלות - הרגע שלפני הרגע הגדול',
   description: 'וילה יוקרתית להתארגנות כלות בהרי ירושלים. חבילת פרימיום עם יין, פינוקים ואווירה מושלמת ליום המיוחד שלך',
   keywords: 'התארגנות כלות, וילה לכלות, חתונה, ירושלים, איפור כלות, סוויטה לכלות',
+  authors: [{ name: 'Multibrawn' }],
   openGraph: {
     title: 'חגית | התארגנות כלות',
-    description: 'וילה יוקרתית בהרי ירושלים להתארגנות הכלה המושלמת',
+    description: 'וילה יוקרתית בהרי ירושלים ליום ההתארגנות המושלם שלך',
     type: 'website',
+    locale: 'he_IL',
   },
 }
 
@@ -32,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="he" dir="rtl">
-      <body className={`${playfair.variable} ${heebo.variable} font-heebo antialiased`}>
+      <body className="font-raleway antialiased">
         {children}
       </body>
     </html>
