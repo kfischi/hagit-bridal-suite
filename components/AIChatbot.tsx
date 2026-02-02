0a650f51ac1e340c1f6b0bfbb305d6830aff9c69
kfischi <kfir.biz@gmail.com>
2026-02-03 00:56:38 +0200

    Update AIChatbot.tsx

--- components/AIChatbot.tsx
+++ components/AIChatbot.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useRef, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import { Bot, X, Send, Sparkles, Phone, Mail } from 'lucide-react'
+import { X, Send, Sparkles, Phone, Mail } from 'lucide-react'
 
 interface Message {
   id: string
