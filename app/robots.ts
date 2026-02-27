37c4f7a005f47a516b3c469a40090e3cdf5ff3f9
kfischi <kfir.biz@gmail.com>
2026-02-27 22:37:32 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,17 +1,19 @@
 import { MetadataRoute } from 'next'
 
-// Required for static export
 export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://hagit-bridal-suite.netlify.app'
+  const base = 'https://suite-hagit.co.il'
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/', '/admin/'],
-    },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/hashaka'],
+      },
+    ],
+    sitemap: `${base}/sitemap.xml`,
+    host: base,
   }
 }
